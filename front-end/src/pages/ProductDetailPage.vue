520d341dc5d0ed4ab9c1994f12822b5782eb13c2
luckyjavid <luckyjavid@gmail.com>
2024-05-09 01:52:05 -0500

    ProductDetailPage, reading url parameter

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,8 +1,38 @@
-<script setup> 
+<script setup>
+  import { products } from '../temp-data';
+  import { ref, computed, onMounted } from 'vue';
+  import { useRoute } from 'vue-router';
 
-</script> 
+  const productId = ref(null);
+  const route = useRoute();
 
-<template> 
-    <h1>Product Detail Page</h1> 
+  onMounted(() => {
+    // Access route params in beforeRouteEnter guard
+    productId.value = route.params.productId;
+  });
+
+  // Use computed property for productDetail
+  const productDetail = computed(() => {
+    if (!productId.value) return null;
+    return products.find(product => product.id === productId.value);
+  });
+</script>
+
+<template>
+  <div>
+    <h1>Product Detail Page</h1>
+    <div class="img-wrap">
+      <img v-if="productDetail" :src="productDetail.imageName" alt="" >
+    </div>
+    <div v-if="productDetail">
+      <div class="product-details">
+        <h2>{{ productDetail.name }}</h2>
+        <h3 class="price">Price: {{ productDetail.price }}</h3>
+        <button class="add-to-cart">Add to Cart</button>
+      </div>
+    </div>
+    <div v-else>
+      <p>Product Not found</p>
+    </div>
+  </div>
 </template>
-
