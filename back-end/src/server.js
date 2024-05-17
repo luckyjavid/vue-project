63d99dc8a8b3ffeede161d4f50d8a7791c8f51ae
luckyjavid <luckyjavid@gmail.com>
2024-05-17 00:21:16 -0500

    server file added to backend with a bunch of api functions to be tested with postman

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,27 +1,37 @@
 import express from 'express'; 
 import { cartItems as cartItemsRaw , products as productsRaw } from './temp-data'
+import { MongoClient } from 'mongodb'
+require('dotenv').config();
 
 let cartItems = cartItemsRaw; 
 let products = productsRaw; 
+const mongodbPassword = process.env.MONGODB_PASSWORD;
 
-const app = express(); 
+//const mongodbPassword = process.env.VUE_APP_MONGODB_PASSWORD;
+const url = `mongodb+srv://javidhussainf:8hBxDku2tZMxyDP5@cluster-jay.bckmk1e.mongodb.net/?retryWrites=true&w=majority&appName=cluster-jay`
+const client = new MongoClient(url); 
+
+const app = express();  
 app.use(express.json());
 
 /* helper function */
-function populateCartIds(ids) {
+function populateCartIds(ids) { 
     return ids.map(id => products.find(product => product.id === id));
 }
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.get('/hello',  (req, res) => {
+    res.send(mongodbPassword);
 })
 
 app.get('/bye', (req, res) => {
     res.send('Ciao!');
 })
 
-app.get('/products', (req, res) => {
-    res.json(products);
+app.get('/products', async (req, res) => {
+    await client.connect(); 
+    const db = client.db('fsv-db'); 
+    const products = await db.collection('products').find({}).toArray(); 
+    res.send(products);
 })
 
 app.get('/shopping-cart', (req, res) => {
--- back-end/src/temp-data.js
+++ back-end/src/temp-data.js
@@ -1,53 +1,43 @@
-
-// import basilBottle from './assets/images/basil-bottle.webp';
-// import lemonBottle from './assets/images/lemon-bottle.webp';
-// import garlicBottle from './assets/images/garlic-bottle.webp';
-// import rosemaryBottle from './assets/images/rosemary-bottle.webp';
-// import chiliBottle from './assets/images/chili-bottle.webp';
-// import delicateBottle from './assets/images/delicate-bottle.webp';
-// import mediumBottle from './assets/images/medium-bottle.webp';
-// import boldBottle from './assets/images/bold-bottle.webp';
-
 export const cartItems = ['123', '234'];
 
 export const products = [{
     id: '123',
     name: 'Basil',
     price: '$26.00',
-    // imageName: basilBottle,
+    imageUrl: '/assets/images/basil-bottle.webp',
 }, {
     id: '234',
     name: 'Lemon',
     price: '$26.00',
-    // imageName: lemonBottle,
+    imageUrl: '/assets/images/lemon-bottle.webp',
 }, {
     id: '345',
     name: 'Garlic',
     price: '$26.00',
-    // imageName: garlicBottle,
+    imageUrl: '/assets/images/garlic-bottle.webp',
 }, {
     id: '456',
     name: 'Rosemary',
     price: '$26.00',
-    // imageName: rosemaryBottle,
+    imageUrl: '/assets/images/rosemary-bottle.webp',
 }, {
     id: '567',
     name: 'Chili',
     price: '$26.00',
-    // imageName: chiliBottle,
+    imageUrl: '/assets/images/chili-bottle.webp',
 }, {
     id: '678',
     name: 'Delicate',
     price: '$24.50',
-    // imageName: delicateBottle,
+    imageUrl: '/assets/images/delicate-bottle.webp',
 }, {
     id: '789',
     name: 'Medium',
     price: '$24.50',
-    // imageName: mediumBottle,
+    imageUrl: '/assets/images/medium-bottle.webp',
 }, {
     id: '890',
     name: 'Bold',
     price: '$24.50',
-    // imageName: boldBottle,
+    imageUrl: '/assets/images/bold-bottle.webp',
 }];
