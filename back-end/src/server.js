477411a8bf8963a5e8f1311ec56ca68ff20042ef
luckyjavid <luckyjavid@gmail.com>
2024-05-17 00:47:16 -0500

    removing redundant db connections

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,61 +5,65 @@ require('dotenv').config();
 
 let cartItems = cartItemsRaw; 
 let products = productsRaw; 
-const mongodbPassword = process.env.MONGODB_PASSWORD;
 
-//const mongodbPassword = process.env.VUE_APP_MONGODB_PASSWORD;
-const url = `mongodb+srv://javidhussainf:8hBxDku2tZMxyDP5@cluster-jay.bckmk1e.mongodb.net/?retryWrites=true&w=majority&appName=cluster-jay`
-const client = new MongoClient(url); 
-
-const app = express();  
-app.use(express.json());
-
-/* helper function */
-function populateCartIds(ids) { 
-    return ids.map(id => products.find(product => product.id === id));
-}
-
-app.get('/hello',  (req, res) => {
-    res.send(mongodbPassword);
-})
-
-app.get('/bye', (req, res) => {
-    res.send('Ciao!');
-})
-
-app.get('/products', async (req, res) => {
+async function start() {
+    // connect to mongo db
+    const mongodbPassword = process.env.MONGODB_PASSWORD;
+    const url = `mongodb+srv://javidhussainf:${mongodbPassword}@cluster-jay.bckmk1e.mongodb.net/?retryWrites=true&w=majority&appName=cluster-jay`;
+    const client = new MongoClient(url); 
     await client.connect(); 
     const db = client.db('fsv-db'); 
-    const products = await db.collection('products').find({}).toArray(); 
-    res.send(products);
-})
 
-app.get('/shopping-cart', (req, res) => {
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-})
+    const app = express();  
+    app.use(express.json());
 
-app.get('/products/:productId', (req, res) => {
-    const productId = req.params.productId;
-    const prodcut = products.find(product => product.id === productId);
-    res.json(prodcut);
-})
+    /* helper function */
+    async function populateCartIds(ids) { 
+        return Promise.all(ids.map( id => db.collection('products').findOne({ id }) ));
+    }
 
+    app.get('/hello',  (req, res) => {
+        res.send('hi');
+    })
 
-app.post('/shopping-cart', (req, res) => {
-    const productId = req.body.id; 
-    cartItems.push(productId);
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-})
+    app.get('/bye', (req, res) => {
+        res.send('Ciao!');
+    })
 
-app.delete('/shopping-cart/:productId', (req, res) => {
-    const productId = req.params.id; 
-    cartItems = cartItems.filter(id => id !== productId); 
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-})
+    app.get('/products', async (req, res) => {
+        const products = await db.collection('products').find({}).toArray(); 
+        res.send(products);
+    })
 
-app.listen(8000,  () => {
-    console.log('Server is listening on port 8000')
-})
+    app.get('/users/:userId/shopping-cart', async (req, res) => {
+        const user = await db.collection('users').findOne({id: req.params.userId}); 
+        const populatedCart = await populateCartIds(user.cartItems);
+        res.json(populatedCart);
+    })
+
+    app.get('/products/:productId', async (req, res) => {
+        const productId = req.params.productId;
+        const prodcut = await db.collection('products').findOne({ id: productId});
+        res.json(prodcut);
+    })
+
+    app.post('/shopping-cart', (req, res) => {
+        const productId = req.body.id; 
+        cartItems.push(productId);
+        const populatedCart = populateCartIds(cartItems);
+        res.json(populatedCart);
+    })
+
+    app.delete('/shopping-cart/:productId', (req, res) => {
+        const productId = req.params.id; 
+        cartItems = cartItems.filter(id => id !== productId); 
+        const populatedCart = populateCartIds(cartItems);
+        res.json(populatedCart);
+    })
+
+    app.listen(8000,  () => {
+        console.log('Server is listening on port 8000')
+    })
+}
+
+start(); 
